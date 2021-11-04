66b8e8a48ab6dbf63ee60873f50c10c04f9f5902
jtandrelevicius <83473629+jtandrelevicius@users.noreply.github.com>
2021-11-04 14:00:49 -0400

    update

--- src/Controller/Controller.Client.pas
+++ src/Controller/Controller.Client.pas
@@ -77,7 +77,7 @@ begin
 
 end;
 
-procedure insertClient(aReq:THorseRequest; aRes:THorseResponse; aNext:TNextProc);
+procedure addClient(aReq:THorseRequest; aRes:THorseResponse; aNext:TNextProc);
 var
   cli : TModelClient;
   objClient : TJSONObject;
@@ -90,16 +90,147 @@ begin
    aRes.Send('Erro ao conectar com o banco de dados').Status(500);
   end;
 
+  try
+   try
+     body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(aReq.Body), 0) as TJsonValue;
+
+     cli.NOME := body.GetValue<string>('nome', '');
+     cli.EMAIL := body.GetValue<string>('email', '');
+     cli.TELEFONE := body.GetValue<string>('telefone', '');
+     cli.CELULAR := body.GetValue<string>('celular', '');
+     cli.CEP := body.GetValue<string>('cep', '');
+     cli.TIPO_LOGRADOURO := body.GetValue<string>('tipo_logradouro', '');
+     cli.LOGRADOURO := body.GetValue<string>('logradouro', '');
+     cli.NUMERO := body.GetValue<string>('numero', '');
+     cli.COMPLEMENTO := body.GetValue<string>('complemento', '');
+     cli.BAIRRO := body.GetValue<string>('bairro', '');
+     cli.UF := body.GetValue<string>('uf', '');
+     cli.CIDADE := body.GetValue<string>('cidade', '');
+     //cli.DATA_CADASTRO := strToDate(body.GetValue<string>('data_cadastro', ''));
+     cli.STATUS := body.GetValue<string>('status', '');
+     cli.CPF_CNPJ := body.GetValue<string>('cpf_cnpj', '');
+     cli.insert(erro);
+
+     body.Free;
+
+     if erro <> '' then
+        raise Exception.Create(erro);
+
+   except on ex:exception do
+   begin
+       aRes.Send(ex.Message).status(400);
+       exit;
+   end;
+   end;
+
+     objClient := TJSONObject.Create;
+     objClient.AddPair('id', cli.ID_CLIENTE.ToString);
+
+     aRes.Send<TJSONObject>(objClient).Status(201);
+ finally
+  cli.Free;
+ end;
 
 end;
 
 procedure deleteClient(aReq:THorseRequest; aRes:THorseResponse; aNext:TNextProc);
+var
+    cli : TModelClient;
+    objClient : TJSONObject;
+    erro : String;
+
 begin
+    // Conexao com o banco...
+     try
+       cli := TModelClient.Create;
+     except
+       aRes.Send('Erro ao conectar com o banco de dados').Status(500);
+       exit;
+     end;
+
+
+    try
+        try
+            cli.ID_CLIENTE := aReq.Params['id'].ToInteger;
+
+            if NOT cli.delete(erro) then
+                raise Exception.Create(erro);
+
+        except on ex:exception do
+            begin
+                aRes.Send(ex.Message).Status(400);
+                exit;
+            end;
+        end;
+
+
+        objClient := TJSONObject.Create;
+        objClient.AddPair('id', cli.ID_CLIENTE.ToString);
+
+        aRes.Send<TJSONObject>(objClient);
+    finally
+        cli.Free;
+    end;
 
 end;
 
 procedure updateClient(aReq:THorseRequest; aRes:THorseResponse; aNext:TNextProc);
+var
+  cli : TModelClient;
+  objClient : TJSONObject;
+  erro : String;
+  body : TJSONValue;
 begin
+    // Conexao com o banco...
+    try
+        cli := TModelClient.Create;
+    except
+        aRes.Send('Erro ao conectar com o banco').Status(500);
+        exit;
+    end;
+
+    try
+        try
+            body := TJSONObject.ParseJSONValue(TEncoding.UTF8.GetBytes(aReq.Body), 0) as TJsonValue;
+
+            cli.ID_CLIENTE := body.GetValue<integer>('id', 0);
+            cli.NOME := body.GetValue<string>('nome', '');
+            cli.EMAIL := body.GetValue<string>('email', '');
+            cli.TELEFONE := body.GetValue<string>('telefone', '');
+            cli.CELULAR := body.GetValue<string>('celular', '');
+            cli.CEP := body.GetValue<string>('cep', '');
+            cli.TIPO_LOGRADOURO := body.GetValue<string>('tipo_logradouro', '');
+            cli.LOGRADOURO := body.GetValue<string>('logradouro', '');
+            cli.NUMERO := body.GetValue<string>('numero', '');
+            cli.COMPLEMENTO := body.GetValue<string>('complemento', '');
+            cli.BAIRRO := body.GetValue<string>('bairro', '');
+            cli.UF := body.GetValue<string>('uf', '');
+            cli.CIDADE := body.GetValue<string>('cidade', '');
+            //cli.DATA_CADASTRO := strToDate(body.GetValue<string>('data_cadastro', ''));
+            cli.STATUS := body.GetValue<string>('status', '');
+            cli.CPF_CNPJ := body.GetValue<string>('cpf_cnpj', '');
+            cli.update(erro);
+
+            body.Free;
+
+            if erro <> '' then
+                raise Exception.Create(erro);
+
+        except on ex:exception do
+            begin
+                aRes.Send(ex.Message).Status(400);
+                exit;
+            end;
+        end;
+
+
+        objClient := TJSONObject.Create;
+        objClient.AddPair('id_cliente', cli.ID_CLIENTE.ToString);
+
+        aRes.Send<TJSONObject>(objClient).Status(200);
+    finally
+        cli.Free;
+    end;
 
 end;
 
@@ -107,9 +238,9 @@ class procedure TControllerClient.Router;
 begin
   THorse.Get('/client', getClient);
   THorse.Get('/client/:id', getClientID);
-  THorse.Post('/client', insertClient);
-  THorse.Delete('/client', deleteClient);
- // THorse.Get('/client', getClient);
+  THorse.Post('/client', addClient);
+  THorse.Put('/client', updateClient);
+  THorse.Delete('/client/:id', deleteClient);
 end;
 
 end.
--- src/Model/Model.Client.pas
+++ src/Model/Model.Client.pas
@@ -23,8 +23,8 @@ type
      FBAIRRO: String;
      FUF: String;
      FCIDADE: String;
-     FDATA_CADASTRO: TDateTime;
-     FDATA_ALTERACAO: TDateTime;
+    // FDATA_CADASTRO: TDateTime;
+    // FDATA_ALTERACAO: TDateTime;
      FUSUARIO_CADASTRO: String;
      FCPF_CNPJ: String;
      FSTATUS: String;
@@ -44,8 +44,8 @@ type
      property BAIRRO: String read FBAIRRO write FBAIRRO;
      property UF: String read FUF write FUF;
      property CIDADE: String read FCIDADE write FCIDADE;
-     property DATA_CADASTRO: TDateTime read FDATA_CADASTRO write FDATA_CADASTRO;
-     property DATA_ALTERACAO: TDateTime read FDATA_ALTERACAO write FDATA_ALTERACAO;
+     //property DATA_CADASTRO: TDateTime read FDATA_CADASTRO write FDATA_CADASTRO;
+     //property DATA_ALTERACAO: TDateTime read FDATA_ALTERACAO write FDATA_ALTERACAO;
      property USUARIO_CADASTRO: String read FUSUARIO_CADASTRO write FUSUARIO_CADASTRO;
      property CPF_CNPJ: String read FCPF_CNPJ write FCPF_CNPJ;
      property STATUS: String read FSTATUS write FSTATUS;
@@ -53,6 +53,7 @@ type
      function getClient(order_by: String; out erro:String): TFDQuery;
      function insert(out erro: String): Boolean;
      function delete(out erro: String): Boolean;
+     function update(out erro: String): Boolean;
   end;
 
 
@@ -66,7 +67,32 @@ Model.Connection.Connect;
 end;
 
 function TModelClient.delete(out erro: String): Boolean;
+var
+    qry : TFDQuery;
 begin
+    try
+        qry := TFDQuery.Create(nil);
+        qry.Connection := Model.Connection.FConnection;
+
+        with qry do
+        begin
+            Active := false;
+            sql.Clear;
+            SQL.Add('DELETE FROM CLIENTES WHERE ID=:ID');
+            ParamByName('ID').Value := ID_CLIENTE;
+            ExecSQL;
+        end;
+
+        qry.Free;
+        erro := '';
+        result := true;
+
+    except on ex:exception do
+        begin
+            erro := 'Erro ao excluir cliente: ' + ex.Message;
+            Result := false;
+        end;
+    end;
 
 end;
 
@@ -116,7 +142,117 @@ begin
 end;
 
 function TModelClient.insert(out erro: String): Boolean;
+var
+    qry : TFDQuery;
 begin
+    // Validacoes...
+    if NOME.IsEmpty then
+    begin
+        Result := false;
+        erro := 'Informe o nome do cliente';
+        exit;
+    end;
+
+    if CELULAR.IsEmpty then
+    begin
+      Result := false;
+      erro := 'Informe o celular do cliente';
+      exit;
+    end;
+
+    try
+        qry := TFDQuery.Create(nil);
+        qry.Connection := Model.Connection.FConnection;
+
+        with qry do
+        begin
+            Active := false;
+            sql.Clear;
+            SQL.Add('INSERT INTO CLIENTES(NOME, EMAIL, TELEFONE, CELULAR, CEP, TIPO_LOGRADOURO, LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO, UF, CIDADE, USUARIO_CADASTRO, CPF_CNPJ, STATUS)');
+            SQL.Add('VALUES(:NOME, :EMAIL, :TELEFONE, :CELULAR, :CEP, :TIPO_LOGRADOURO, :LOGRADOURO, :NUMERO, :COMPLEMENTO, :BAIRRO, :UF, :CIDADE, :USUARIO_CADASTRO, :CPF_CNPJ, :STATUS)');
+
+            ParamByName('NOME').Value := NOME;
+            ParamByName('EMAIL').Value := EMAIL;
+            ParamByName('TELEFONE').Value := TELEFONE;
+            ParamByName('CELULAR').Value := CELULAR;
+            ParamByName('CEP').Value := CEP;
+            ParamByName('TIPO_LOGRADOURO').Value := TIPO_LOGRADOURO;
+            ParamByName('LOGRADOURO').Value := LOGRADOURO;
+            ParamByName('NUMERO').Value := NUMERO;
+            ParamByName('COMPLEMENTO').Value := COMPLEMENTO;
+            ParamByName('BAIRRO').Value := BAIRRO;
+            ParamByName('UF').Value := UF;
+            ParamByName('CIDADE').Value := CIDADE;
+            //ParamByName('DATA_CADASTRO').Value := DATA_CADASTRO;
+            ParamByName('USUARIO_CADASTRO').Value := USUARIO_CADASTRO;
+            ParamByName('CPF_CNPJ').Value := CPF_CNPJ;
+            ParamByName('STATUS').Value := STATUS;
+
+            ExecSQL;
+
+            // Busca ID inserido...
+            Params.Clear;
+            SQL.Clear;
+            SQL.Add('SELECT MAX(ID) AS ID FROM CLIENTES');
+            SQL.Add('WHERE CELULAR=:CELULAR');
+            ParamByName('CELULAR').Value := CELULAR;
+            active := true;
+
+            ID_CLIENTE := FieldByName('ID').AsInteger;
+        end;
+
+        qry.Free;
+        erro := '';
+        result := true;
+
+    except on ex:exception do
+        begin
+            erro := 'Erro ao cadastrar cliente: ' + ex.Message;
+            Result := false;
+        end;
+    end;
+
+end;
+
+function TModelClient.update(out erro: String): Boolean;
+var
+    qry : TFDQuery;
+begin
+    // Validacoes...
+    if ID_CLIENTE <= 0 then
+    begin
+        Result := false;
+        erro := 'Informe o id. cliente';
+        exit;
+    end;
+
+    try
+        qry := TFDQuery.Create(nil);
+        qry.Connection := Model.Connection.FConnection;
+
+        with qry do
+        begin
+            Active := false;
+            sql.Clear;
+            SQL.Add('UPDATE TAB_CLIENTE SET NOME=:NOME, EMAIL=:EMAIL, FONE=:FONE');
+            SQL.Add('WHERE ID_CLIENTE=:ID_CLIENTE');
+            ParamByName('NOME').Value := NOME;
+            ParamByName('EMAIL').Value := EMAIL;
+            //ParamByName('FONE').Value := FONE;
+            ParamByName('ID_CLIENTE').Value := ID_CLIENTE;
+            ExecSQL;
+        end;
+
+        qry.Free;
+        erro := '';
+        result := true;
+
+    except on ex:exception do
+        begin
+            erro := 'Erro ao alterar cliente: ' + ex.Message;
+            Result := false;
+        end;
+    end;
 
 end;
 
